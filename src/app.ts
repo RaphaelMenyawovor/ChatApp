caf9d32211a3702f649dab3f867a2bc62c6ec392
Raphael Menyawovor <raphaelmenyawovor@gmail.com>
2026-03-07 06:39:15 +0000

    chore: clean up app.ts by removing unused path import and adding spacing

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { type Express} from 'express';
-import path from 'path';
 
 const app: Express = express();
 
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
+
+
+
 
 export default app;
