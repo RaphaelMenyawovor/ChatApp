50ac0d65c4784789af6e2f626531706dfc93da24
Raphael Menyawovor <raphaelmenyawovor@gmail.com>
2026-03-07 17:15:36 +0000

    feat: update server port to 8080, improve static file serving path in app.ts, and add Prisma client dependency

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express, { type Express} from 'express';
+import path from 'path';
 
 const app: Express = express();
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 export default app;
