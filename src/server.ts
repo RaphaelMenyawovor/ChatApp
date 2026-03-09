2bd88183b913e3ab54ae19b01de87461c5ece56e
Raphael Menyawovor <raphaelmenyawovor@gmail.com>
2026-03-09 01:59:57 +0000

    feat: initialize Socket.IO server in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import app from './app.js';
 import "dotenv/config";
+import { Socket } from "socket.io";
 
 const PORT = process.env.PORT || 3000;
+
+// Initialize Socket.IO
+const io = new Socket.Server(app);
+
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
